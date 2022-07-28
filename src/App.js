98b745e3328389ded1baf506c604e3e0a6165093
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-07-28 05:58:54 +0300

    checks added

--- src/App.js
+++ src/App.js
@@ -24,6 +24,20 @@ const App = () => {
       }
     }
   }
+
+  const checkForRowOfFour = () => {
+    for (let i = 0; i < 64; i++){
+      const rowOfFour = [i, i + 1, i + 2, i + 3]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+
+      if(notValid.includes(i))continue
+
+      if(rowOfFour.every(square => currentColorArrangement[square] === decidedColor)){
+        rowOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
   
   const checkForColumnOfThree = () => {
     for (let i = 0; i < 47; i++){
@@ -32,6 +46,20 @@ const App = () => {
 
       if(columnOfThree.every(square => currentColorArrangement[square] === decidedColor)){
         columnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++){
+      const rowOfThree = [i, i + 1, i + 2]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39,46, 47, 54, 55, 63,64]
+
+      if(notValid.includes(i))continue
+
+      if(rowOfThree.every(square => currentColorArrangement[square] === decidedColor)){
+        rowOfThree.forEach(square => currentColorArrangement[square] = '')
       }
     }
   }
@@ -55,14 +83,16 @@ const App = () => {
 
     const timer = setInterval(() =>{
       checkForColumnOfFour()
+      checkForRowOfFour()
       checkForColumnOfThree()
+      checkForRowOfThree()
       
       setCurrentColorArrangement([...currentColorArrangement])
     }, 100)
     
     return () => clearInterval(timer)
 
-  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement])
+  }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, currentColorArrangement])
   
   console.log(currentColorArrangement) 
 
