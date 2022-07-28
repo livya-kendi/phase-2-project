f32f1922c865769c7f429f58443e5f2e0d764ec2
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-07-28 03:42:13 +0300

    board created

--- src/App.js
+++ src/App.js
@@ -5,11 +5,23 @@ const candyColours = [
   'orange',
   'purple',
   'red',
-  'yellow',
+  'yellow'
 ]
 
 
-function App() {
+const App = () => {
+
+  const createBoard = () => {
+    //set the random numbers to pass from 0 to 5 into the candy colours
+    const randomColourArrangement = []
+    for (let i = 0; i < width*width; i++){
+      const randomColour = candyColours[Math.floor(Math.random() * candyColours.length)]
+      randomColourArrangement.push(randomColour)
+    }
+    console.log(randomColourArrangement)
+  }
+  createBoard()
+
   return (
     <div> </div>
   );
